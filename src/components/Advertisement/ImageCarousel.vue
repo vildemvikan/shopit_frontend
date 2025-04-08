0cfbaa62b5919f8c7494c0e91898a28e3e7b7c87
vildemv <vildemv@ntnu.no>
2025-04-08 13:43:35 +0200

    Added bookmark to advertisement view and did also change from local interfaces to imported global interfaces.

--- src/components/Advertisement/ImageCarousel.vue
+++ src/components/Advertisement/ImageCarousel.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import type { Status } from '@/enums/enums.ts'
+import {Image} from '@/interfaces/interfaces.ts'
+import { createBookmark, deleteBookmark } from '../../../utils/Bookmark.ts'
+import { useRouter } from 'vue-router'
 
 // Destructure the images prop from defineProps
-const { images } = defineProps<{ images: Image[] , status: Status}>()
+const props = defineProps<{
+  id: string
+  images: Image[] ,
+  status: Status,
+  isOwner: boolean,
+  isBookmarked: boolean
+}>()
 
+const router = useRouter()
+
+const bookmarked = ref<boolean>(props.isBookmarked)
 // Use a more descriptive name for the selected index
 const selectedIndex = ref<number>(0)
 // Reactive variable to control modal visibility
@@ -16,7 +29,6 @@ function previousImage(){
     selectedIndex.value = images.length - 1
   }
 }
-
 function nextImage(){
   if(selectedIndex.value < images.length - 1){
     selectedIndex.value += 1
@@ -24,13 +36,36 @@ function nextImage(){
     selectedIndex.value = 0
   }
 }
-
 function openModal() {
   showModal.value = true
 }
-
 function closeModal() {
   showModal.value = false
+}
+
+async function bookmarkItem(){
+  try{
+    const result = await createBookmark(props.id)
+    if(result == 401){
+      await router.push('/auth')
+    }
+    bookmarked.value = result === 200
+
+  } catch (error){
+    bookmarked.value = false
+  }
+}
+
+async function removeBookmark(){
+  try{
+    const result = await deleteBookmark(props.id)
+    if(result == 401){
+      await router.push('/auth')
+    }
+    bookmarked.value = !(result == 204)
+  } catch (error){
+    bookmarked.value = true
+  }
 }
 </script>
 
@@ -67,7 +102,7 @@ function closeModal() {
         <div class="modal-content">
           <img :src="images[selectedIndex].url" alt="Large Image" />
 
-          <div class="rotate-buttons">
+          <div class="rotate-buttons" v-if="images.length > 1">
             <button @click="previousImage" class="rotate-button" id="rotate-button-modular">
               <img src="@/assets/icons/up.svg" class="rotate-icon" id="previous" alt="Previous image"/>
             </button>
@@ -75,12 +110,13 @@ function closeModal() {
               <img src="@/assets/icons/up.svg" class="rotate-icon" id="next" alt="Next Image"/>
             </button>
           </div>
+
           <button class="close-button" @click="closeModal">Close</button>
         </div>
       </div>
     </transition>
 
-    <div class="rotate-buttons">
+    <div class="rotate-buttons" v-if="images.length > 1">
       <button @click="previousImage" class="rotate-button">
         <img src="@/assets/icons/up.svg" class="rotate-icon" id="previous" alt="Previous image"/>
       </button>
@@ -88,6 +124,11 @@ function closeModal() {
         <img src="@/assets/icons/up.svg" class="rotate-icon" id="next" alt="Next Image"/>
       </button>
     </div>
+    <img src="@/assets/icons/bookmarkNotMarked.svg"
+         alt="bookmark" class="bookmark" v-if="!bookmarked && !isOwner" @click.stop="bookmarkItem"/>
+    <img src="@/assets/icons/bookmarkMarked.svg"
+         alt="bookmark" class="bookmark" v-if="bookmarked && !isOwner" @click.stop="removeBookmark"/>
+
   </div>
 </template>
 
@@ -208,6 +249,20 @@ function closeModal() {
   max-width: 5%;
 }
 
+.bookmark{
+  position: absolute;
+  top: 5px;
+  right: 0;
+  height: 12%;
+  z-index: 10;
+  cursor: pointer;
+}
+
+.bookmark:hover{
+  transform: scale(1.01);
+}
+
+
 .rotate-icon{
   max-width: 75%;
 }
@@ -266,7 +321,6 @@ function closeModal() {
     aspect-ratio: 1/1;
   }
 }
-
 
 @media (max-width: 700px){
   .images{
