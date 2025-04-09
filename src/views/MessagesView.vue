df61023e5f562c3c535d1a316b4ef9e403919504
Harry Linrui XU <xulr0820@hotmail.com>
2025-04-09 17:03:34 +0200

    feat(chat-room): fix notifaaction label

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -2,9 +2,10 @@
 
 import MessageList from '@/components/Messages/ChatList.vue'
 import Chat from '@/components/Messages/Chat.vue'
-import { computed, onMounted, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref, watch } from 'vue'
 import { fetchChatList } from '../../utils/Messages.ts'
 import type { ChatCardInfo, ChatRoomInfo } from '@/interfaces/interfaces.ts'
+import useEventsBus from '../../utils/EventBus.ts'
 
 const chatList = ref<ChatCardInfo[]>([]);
 let currentChatRoomInfo = reactive<ChatRoomInfo>({
@@ -15,6 +16,16 @@ let currentChatRoomInfo = reactive<ChatRoomInfo>({
 );
 
 const hasSelectedMessage = ref(false);
+
+const { bus } = useEventsBus();
+
+watch(()=> bus.value.get('selectChat'), () => {
+  hasSelectedMessage.value = true;
+})
+
+watch(()=> bus.value.get('closeChat'), () => {
+  hasSelectedMessage.value = false;
+})
 
 const isChatEmpty = computed (()=> {
   return chatList.value.length === 0;
@@ -31,13 +42,15 @@ onMounted(async ()=> {
   <h2>{{ $t('messages') }}</h2>
 
  <div class="container">
-   <div v-if="!isChatEmpty" class="chat-list-wrapper">
+   <div v-if="!isChatEmpty" class="chat-list-wrapper"
+        :class="!hasSelectedMessage ? 'selected' : 'unselected'">
      <message-list
        :current-user="currentChatRoomInfo.senderMail"
      ></message-list>
    </div>
-   <button>back</button>
-   <div v-if="!isChatEmpty" class="chat-wrapper">
+   <button @click="hasSelectedMessage = false"></button>
+   <div v-if="!isChatEmpty" class="chat-wrapper"
+        :class="hasSelectedMessage ? 'selected' : 'unselected'">
      <Chat></Chat>
    </div>
  </div>
@@ -63,13 +76,13 @@ onMounted(async ()=> {
   max-width: 50%;
 }
 
-@media (max-width: 8000px) {
-
-  .chat-list-wrapper, .chat-wrapper{
+@media (max-width: 800px) {
+  .selected {
     width: 100%;
     max-width: 100%;
   }
+  .unselected {
+    display:none
+  }
 }
-
-
 </style>
