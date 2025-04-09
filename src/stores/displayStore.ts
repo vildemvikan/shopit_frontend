f199906c5336c699aed700ad295d356f5a2ba2e8
vildemv <vildemv@ntnu.no>
2025-04-09 10:24:32 +0200

    Created store for saving color and language preferences

---
+++ src/stores/displayStore.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { Condition } from '@/stores/advertisementStore.ts'
+
+export const displayStore = defineStore('display', {
+  state: () => ({
+    language: null as string|null,
+    mode: null as string | null
+  }),
+
+  actions: {
+    updateLanguage(newLanguage: string) {
+      this.language = newLanguage
+    },
+    updateMode(newMode: string) {
+      this.mode = newMode
+    },
+  },
+  persist: true
+})
